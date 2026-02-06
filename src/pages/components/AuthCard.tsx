9f7d96edc094dd88bc6e2d5a4ac36eccff14e127
CatsVend <juwon1202@icloud.com>
2026-02-06 21:21:15 +0900

    init

---
+++ src/pages/components/AuthCard.tsx
@@ -0,0 +1,20 @@
+﻿import type { ReactNode } from 'react'
+import { Card, CardBody, CardHeader, CardSubtitle, CardTitle } from './ops/AuthCard.styles'
+
+type AuthCardProps = {
+  title: string
+  subtitle: string
+  children: ReactNode
+}
+
+const AuthCard = ({ title, subtitle, children }: AuthCardProps) => (
+  <Card>
+    <CardHeader>
+      <CardTitle>{title}</CardTitle>
+      <CardSubtitle>{subtitle}</CardSubtitle>
+    </CardHeader>
+    <CardBody>{children}</CardBody>
+  </Card>
+)
+
+export default AuthCard
