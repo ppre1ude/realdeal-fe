62aaba55d836822f10a7079dba85d9795e365041
ppre1ude <cjh5110@naver.com>
2026-02-06 22:02:11 +0900

    feat: Home html 기반 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 ﻿import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import HomePage from './pages/Home'
 import LoginPage from './pages/login'
 import RegisterPage from './pages/register'
 
 const App = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
-      <Route path="*" element={<Navigate to="/login" replace />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   </BrowserRouter>
 )
